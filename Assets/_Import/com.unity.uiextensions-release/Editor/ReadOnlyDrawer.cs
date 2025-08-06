0656405ee8713cf15bbb4c86ccc536bb91694914
Bạch Thành Điền <diendienbach@gmail.com>
2025-08-06 23:02:01 +0700

    V_1.0.1_Import assets

---
+++ Assets/_Import/com.unity.uiextensions-release/Editor/ReadOnlyDrawer.cs
@@ -0,0 +1,22 @@
+﻿/// Credit tanoshimi
+/// Sourced from - https://forum.unity3d.com/threads/read-only-fields.68976/
+/// 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace UnityEngine.UI.Extensions
+{ 
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
+#if UNITY_EDITOR
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+        }
+#endif
+    }
+}
